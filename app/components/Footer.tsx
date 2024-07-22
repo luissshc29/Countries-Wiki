c864dc10b4584d91572a43e446014f9d5a041626
Luis Henrique <luishcc2003@gmail.com>
2024-07-21 21:53:55 -0300

    Adicionando link para o portfolio no footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,26 +1,39 @@
 import React from "react";
-import { AiOutlineGithub, AiOutlineInstagram } from "react-icons/ai";
-
-import { FaLinkedin } from "react-icons/fa";
+import {
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+  AiOutlineGithub,
+} from "react-icons/ai";
+import { FiExternalLink } from "react-icons/fi";
 
 export default function Footer() {
-    return (
-        <footer className="w-full flex justify-center items-center gap-2 h-[10vh] mt-12 shadow-top dark:shadow-neutral-900 shadow-neutral-200">
-            <p className="text-xs md:text-sm font-semibold">
-                Feito por Luis Henrique
-            </p>
-            <p className="text-xl">|</p>
-            <div className="flex gap-1 items-center text-sm font-semibold">
-                <a href="https://www.instagram.com/luissshc_/">
-                    <AiOutlineInstagram />
-                </a>
-                <a href="https://www.linkedin.com/in/luis-henrique-6a7425165/">
-                    <FaLinkedin />
-                </a>
-                <a href="https://github.com/luissshc29">
-                    <AiOutlineGithub />
-                </a>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="flex justify-center items-center gap-2 lg:col-span-2 mt-auto mb-0 py-6 w-full text-neutral-700 text-sm dark:text-neutral-400">
+      <div className="flex items-center gap-[3px] h-full">
+        Coded by{" "}
+        <a
+          href="https://luishenrique-dev.com.br/"
+          className="flex items-center gap-[3px] underline"
+          target="_blank"
+        >
+          Luis Henrique <FiExternalLink />
+        </a>{" "}
+        |
+      </div>
+      <div className="flex gap-2">
+        <a href="https://www.instagram.com/luissshc_/" target="_blank">
+          <AiOutlineInstagram />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/luis-henrique-6a7425165/"
+          target="_blank"
+        >
+          <AiOutlineLinkedin />
+        </a>
+        <a href="https://github.com/luissshc29" target="_blank">
+          <AiOutlineGithub />
+        </a>
+      </div>
+    </footer>
+  );
 }
